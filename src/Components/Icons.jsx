5867d6546869c46ae292a085d0ee976aa2b9fc21
jryambao <johnralph266@gmail.com>
2022-09-05 14:02:59 +0800

    qewq

---
+++ src/Components/Icons.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../icons.css'
+
+const Icons = () => {
+  return (
+    <div class="pl-12 invisible lg:visible">
+        <ul class="social_icons">
+            <li> <a href="https://www.facebook.com/yambaosan"><i className="font-bold fa fa-facebook" aria-hidden="true"></i></a> </li>
+            <li> <a href="https://github.com/jryambao"><i className="font-bold fa fa-github" aria-hidden="true"></i></a> </li>
+            <li> <a href="https://www.linkedin.com/in/john-ralph-y-595473137/"><i className="font-bold fa fa-linkedin" aria-hidden="true"></i></a> </li>
+            <li> <a href="mailto: johnralph266@gmail.com"><i className="font-bold fa fa-google" aria-hidden="true"></i></a> </li>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Icons
